d2ed017e7a254ee7ad9ef084cf776f25eac6016a
WoodrowCrawford <woodrowcrawford03@gmail.com>
2021-02-03 16:57:43 -0600

    Added comments  Listed Lists

--- LinkedList.h
+++ LinkedList.h
@@ -13,7 +13,7 @@ public:
     T data();
 };
 
-/////////////////////////////
+///////////////////////////////
 
 
 //Iterator class
@@ -81,13 +81,7 @@ public:
 
     const LinkedList<T>& operator=(const LinkedList<T>& otherLinkedList);
 
-   
-
-    
-
 };
-
-
 
 
 
@@ -96,18 +90,41 @@ public:
 template<typename T>
 inline Node<T>::Node()
 {
+    Node* node;
+    node->data[0];
+    node->data[1];
+    node->data[2];
+    node->data[3];
+
+
 }
+
+
+
 
 template<typename T>
 inline Node<T>::Node(T value)
 {
+    Node* node;
+    node->data[0] = "John";
+    node->data[1] = "Danny";
+    node->data[2] = "Josa";
+    node->data[3] = "Mike";
+    
+
 }
+
+
+
 
 template<typename T>
 inline Node<T>* Node<T>::next()
 {
-    return NULL;
+    return
 }
+
+
+
 
 template<typename T>
 inline Node<T>* Node<T>::previous()
@@ -115,13 +132,16 @@ inline Node<T>* Node<T>::previous()
     return NULL;
 }
 
+
+
+
 template<typename T>
 inline T Node<T>::data()
 {
     return T();
 }
 
-///////////////////////////
+//////////////////////////////////
 
 
 
@@ -148,95 +168,174 @@ inline const bool Iterator<T>::operator!=(const Iterator<T>& iter)
     return false;
 }
 
-///////////////////////////////
+///////////////////////////////////
 
 
 //Linked List class
 template<typename T>
 inline LinkedList<T>::LinkedList()
 {
+
+
 }
 
 template<typename T>
 inline LinkedList<T>::LinkedList(LinkedList<T>&)
 {
+
+
 }
 
 template<typename T>
 inline LinkedList<T>::~LinkedList()
 {
+
+
 }
 
+
+//Deletes all nodes in the list
 template<typename T>
 inline void LinkedList<T>::destroy()
 {
+
+
 }
 
+
+//Returns an iterator pointing to the first node in the list
 template<typename T>
 inline Iterator<T> LinkedList<T>::begin()
 {
     return Iterator<T>();
 }
 
+
+
+//Returns the next item after the last node in the list
 template<typename T>
 inline Iterator<T> LinkedList<T>::end()
 {
     return Iterator<T>();
 }
 
+
+
+//Adds a new node at the given index
+template<typename T>
+inline bool LinkedList<T>::insert(const T& value, int index)
+{
+    return false;
+}
+
+
+
+//Remove all nodes with the given value
 template<typename T>
 inline bool LinkedList<T>::remove(const T& value)
 {
     return false;
 }
 
+
+
+//Sets the given iterator to point to a node at the given index
 template<typename T>
 inline bool LinkedList<T>::getData(Iterator<T>& Iter, int index)
 {
     return false;
 }
 
+
+
+//Returns whether or not the list has any nodes in it
 template<typename T>
 inline const bool LinkedList<T>::isEmpty()
 {
     return false;
 }
 
+
+
+//Checks to see if the given item is in the list
 template<typename T>
 inline const bool LinkedList<T>::contains(const T& object)
 {
     return false;
 }
 
+
+
+//Adds a new node to the beginning of the list
 template<typename T>
 inline void LinkedList<T>::pushFront(const T& value)
 {
+
+
 }
 
+
+
+//Adds a new node to the end of the list
 template<typename T>
 inline void LinkedList<T>::pushBack(const T& value)
 {
+
+
 }
 
+
+
+
+template<typename T>
+inline void LinkedList<T>::sort()
+{
+
+
+
+}
+
+
+
+//Set the default values for the first node pointer,
+//the last node pointer, and the node count
 template<typename T>
 inline void LinkedList<T>::initialize()
 {
+
+
 }
 
+
+
+//Prints the values for all the nodes
 template<typename T>
 inline const void LinkedList<T>::print()
 {
     return void();
 }
 
+
+
+//Returns the anount of nodes in the list
 template<typename T>
 inline const int LinkedList<T>::getLenght()
 {
     return 0;
 }
 
+
+
 template<typename T>
 inline const LinkedList<T>& LinkedList<T>::operator=(const LinkedList<T>& otherLinkedList)
 {
     // TODO: insert return statement here
+}
+
+/////////////////////////////////////////////
+
+
+int main()
+{
+    
 }
